92916188fe35aed0b0aa343782b1572e25bca410
Matias Cardenas <cardenasmatias.1990@gmail.com>
2024-02-11 00:04:15 +0100

    feat: adding tasks class-based views and templates

--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import ListView
+from tasks.models import Task
 
-# Create your views here.
+
+class TaskListView(ListView):
+    model = Task
+    template_name = "task_list.html"
+    context_object_name = "tasks"
+
+
+class TaskDetailView(ListView):
+    model = Task
+    template_name = "task_detail.html"
+    context_object_name = "task"
+
+
+class TaskCreateView(ListView):
+    model = Task
+    template_name = "task_form.html"
+    fields = ("name", "description", "start_date", "end_date")
+
+    def get_success_url(self):
+        return reverse("task-detail", kwargs={"pk": self.object.pk})
+
+
+class TaskUpdateView(ListView):
+    model = Task
+    template_name = "task_form.html"
+    fields = ("name", "description", "start_date", "end_date")
+
+    def get_success_url(self):
+        return reverse("task-detail", kwargs={"pk": self.object.pk})
+
+
+class TaskDeleteView(ListView):
+    model = Task
+    template_name = "task_confirm_delete.html"
+
+    def get_success_url(self):
+        return reverse("task-list")
