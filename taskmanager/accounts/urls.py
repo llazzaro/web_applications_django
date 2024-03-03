80b4033f29a9731eba80843100eec458d4adf274
Matias Cardenas <cardenasmatias.1990@gmail.com>
2024-03-03 11:23:35 +0100

    feat: adding password change & reset functionality

--- taskmanager/accounts/urls.py
+++ taskmanager/accounts/urls.py
@@ -1,6 +1,14 @@
 from accounts import views
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeView,
+    PasswordResetCompleteView,
+    PasswordResetConfirmView,
+    PasswordResetDoneView,
+    PasswordResetView,
+)
+from django.urls import path, reverse_lazy
 
 app_name = "accounts"
 
@@ -8,4 +16,37 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("login/", LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path(
+        "password_change/",
+        PasswordChangeView.as_view(
+            success_url=reverse_lazy("accounts:password_change_done"),
+            template_name="accounts/password_change_form.html",
+        ),
+        name="password_change",
+    ),
+    path(
+        "password_change/done/",
+        PasswordChangeView.as_view(template_name="accounts/password_change_done.html"),
+        name="password_change_done",
+    ),
+    path(
+        "password_reset/",
+        PasswordResetView.as_view(email_template_name="accounts/custom_password_reset_email.html"),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"),
+        name="password_reset_done",
+    ),
+    path(
+        "password_reset/confirm/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(template_name="accounts/password_reset_confirm.html"),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password_reset/complete/",
+        PasswordResetCompleteView.as_view(template_name="accounts/password_reset_complete.html"),
+        name="password_reset_complete",
+    ),
 ]
--- taskmanager/taskmanager/urls.py
+++ taskmanager/taskmanager/urls.py
@@ -16,24 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.contrib.auth.views import PasswordChangeView
-from django.urls import include, path, reverse_lazy
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("tasks/", include("tasks.urls", namespace="tasks")),
     path("accounts/", include("accounts.urls", namespace="accounts")),
-    path(
-        "password_change/",
-        PasswordChangeView.as_view(
-            success_url=reverse_lazy("accounts:password_change_done"),
-            template_name="accounts/password_change_form.html",
-        ),
-        name="password_change",
-    ),
-    path(
-        "password_change/done/",
-        PasswordChangeView.as_view(template_name="accounts/password_change_done.html"),
-        name="password_change_done",
-    ),
 ]
